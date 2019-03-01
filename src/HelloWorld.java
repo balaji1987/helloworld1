128e51accc13c4d9890c126bf30c15f53d904415
Balaji S <Balaji.S@unisys.com>
2019-03-01 15:38:22 +0530

    xxx-will fails

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.prntln("Hlo, World");
+        Systm.out.prntln("Hlo, World");
     }
 
 }
